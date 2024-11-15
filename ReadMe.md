ce1d2404a0b5ad647d91b7f54d42229dedabe34c
Nelida <neessu2023@gmail.com>
2024-11-14 22:23:22 -0300

    creacion de imagen de docker

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+link de docker : https://hub.docker.com/u/nellysu
+link de github : https://github.com/NelidaSuarez/Adoptme
+
+generado los test 
+creado el swagger
+he enlasado docker a mongo sin problemas 
+
+envio datos de conecciones por mensaje ,
+
+
+
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,7 @@ import { dirname } from 'path';
 export const createHash = async(password) =>{
     const salts = await bcrypt.genSalt(10);
     return bcrypt.hash(password,salts);
+    
 }
 
 export const passwordValidation = async(user,password) => bcrypt.compare(password,user.password);
